64c5b5eac0b559dbd00ef9d2571160a41d511147
Aarchie05 <68143839+Aarchie05@users.noreply.github.com>
2021-02-22 21:23:56 +0530

    WEEK 4

---
+++ Week 4/Question2.java
@@ -0,0 +1,14 @@
+// The following is the declaration of the main class named Question42
+public class Question42 
+{ 
+	public static void main(String args[])
+    {
+		int year; // integer type variable to store year
+        java.util.Calendar calendar = java.util.Calendar.getInstance();  
+        year= calendar.get(java.util.Calendar.YEAR);
+        // Print the current Year
+        System.out.println("Current Year: "+year);
+        //int month = current.get(current.MONTH);
+        System.out.print("sum: "+ 5);
+  }  
+}
---
+++ Week 4/Question4.java
@@ -0,0 +1,23 @@
+interface First{ 
+    default void show(){ 
+        System.out.println("Default method implementation of First interface."); 
+    } 
+} 
+  
+interface Second{ 
+    default void show(){ 
+        System.out.println("Default method implementation of Second interface."); 
+    } 
+} 
+  
+// Implementation class code 
+class Question44 implements First, Second{ 
+    public void show(){
+        First.super.show();
+        Second.super.show();
+    } 
+    public static void main(String args[]){ 
+        Question44 q = new Question44(); 
+        q.show(); 
+    } 
+}
