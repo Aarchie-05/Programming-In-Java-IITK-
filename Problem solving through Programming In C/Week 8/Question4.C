10ac16c22918f4ca878088dfd57e9b3c2bea1090
Aarchie <aarchie.girdhar10@gmail.com>
2021-03-13 23:32:09 +0530

    Update Question4.C

---
+++ Problem solving through Programming In C/Week 8/Question4.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+void reverse(char[], int, int);
+    int main()
+    {
+        char str1[20];
+        scanf("%s", str1); //The string is taken as input form the test data. 
+     
+/* Complete the program to print the string in reverse order using the function
+void reverse(char[], int, int);
+Use the printf statement as
+printf("The string after reversing is: %s\n", str1); 
+You can use different variable also.
+*/
