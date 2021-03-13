9ee91fdf2dd814f4ffb454269913694fdccc6bc9
Aarchie <aarchie.girdhar10@gmail.com>
2021-03-13 23:35:08 +0530

    Update Question4.C

--- Problem solving through Programming In C/Week 8/Question4.C
+++ Problem solving through Programming In C/Week 8/Question4.C
@@ -13,3 +13,18 @@ Use the printf statement as
 printf("The string after reversing is: %s\n", str1); 
 You can use different variable also.
 */
+void reverse(char str1[], int i, int len)
+{
+  int c,j;
+
+  j = len - (i + 1);
+    
+    if( i < j )
+    {
+        c = str1[i];
+        str1[i] = str1[j];
+        str1[j] = c;
+
+        reverse(str1,++i,len);
+    }
+}
