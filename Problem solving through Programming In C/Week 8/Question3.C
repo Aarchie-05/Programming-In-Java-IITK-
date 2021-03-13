74aa4166e4c06151d4df9af9b75f1c642aea390b
Aarchie <aarchie.girdhar10@gmail.com>
2021-03-13 23:30:53 +0530

    Update Question3.C

--- Problem solving through Programming In C/Week 8/Question3.C
+++ Problem solving through Programming In C/Week 8/Question3.C
@@ -9,3 +9,11 @@ int main()
   printf("The binary equivalent of %d is %d\n", num, bin);
   return 0;
   }
+int binary_conversion(int num)
+{
+  
+   if (num == 0)  
+        return 0;  
+    else
+        return (num % 2 + 10 * binary_conversion(num / 2)); 
+}
