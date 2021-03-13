fd2a1c8e9a0e02240c32b691a559bd58e3f1da6b
Aarchie <aarchie.girdhar10@gmail.com>
2021-03-13 23:25:38 +0530

    Update Question2.C

--- Problem solving through Programming In C/Week 8/Question2.C
+++ Problem solving through Programming In C/Week 8/Question2.C
@@ -12,3 +12,11 @@ result = power(num, pow);
 printf("%d^%d is %ld", num, pow, result);
 return 0;
 }
+long power (int num, int pow)
+{
+    if (pow)
+    {
+        return (num * power(num, pow - 1));
+    }
+    return 1;
+}
