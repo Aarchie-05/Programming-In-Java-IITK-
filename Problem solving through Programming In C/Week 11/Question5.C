65a0972f4003d602603c6e2b29c9a530e93cee8f
Aarchie <aarchie.girdhar10@gmail.com>
2021-04-17 14:47:09 +0530

    Add files via upload

---
+++ Problem solving through Programming In C/Week 11/Question5.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ 
+int prime(int num); //Function to find whether the number is prime or not.
+int main() {
+   int lines;
+   scanf("%d", &lines); //Number of lines of the triangle is taken from test data.
+
+//use the printf statement as printf("%d\t", variable_name); to print the elements in a row
+
+ 
